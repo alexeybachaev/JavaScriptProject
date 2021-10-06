b36508d4193ca629e35919200e4bda74140ea1f9
alexeybachaev <bachaev2000@yandex.ru>
2021-10-06 22:40:25 +0300

    create posts from firebase db

--- services/api.service.js
+++ services/api.service.js
@@ -16,6 +16,19 @@ class ApiService {
             console.error(error)
         }
     }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            })
+            const responce = await fetch(request)
+            return await responce.json()
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 export const apiService = new ApiService('https://somejsproject-js-default-rtdb.europe-west1.firebasedatabase.app')
